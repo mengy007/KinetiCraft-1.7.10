03084e2005bee3712964963df96aa413fb335be2
Meng Yang <myang@gpstrackit.net>
2014-08-01 11:51:32 -0700

    Code clean up

--- src/main/java/com/techmafia/mcmods/KinetiCraft/gui/GuiStoneKineticEnergyCube.java
+++ src/main/java/com/techmafia/mcmods/KinetiCraft/gui/GuiStoneKineticEnergyCube.java
@@ -17,34 +17,12 @@ import cpw.mods.fml.relauncher.Side;
 import cpw.mods.fml.relauncher.SideOnly;
 
 @SideOnly(Side.CLIENT)
-public class GuiStoneKineticEnergyCube extends GuiContainer
+public class GuiStoneKineticEnergyCube extends GuiBaseEnergyCube
 {
 	public GuiStoneKineticEnergyCube(Container container)
 	{
 		super(container);
+		this.inventoryName		= "Stone Kinetic Energy Cube";
+		this.inventoryTexture 	= "kineticraft:textures/gui/stoneKineticEnergyCubeGui.png";
 	}
-
-	/**
-     * Draw the foreground layer for the GuiContainer (everything in front of the items)
-     */
-    protected void drawGuiContainerForegroundLayer(int par1, int par2)
-    {
-        this.fontRendererObj.drawString("Stone Kinetic Energy Cube", 7, 6, 4210752);
-        this.fontRendererObj.drawString("Inventory", 7, 71, 4210752);
-    }
-
-    /**
-     * Draw the background layer for the GuiContainer (everything behind the items)
-     */
-    protected void drawGuiContainerBackgroundLayer(float par1, int par2, int par3)
-    {
-    	
-        GL11.glColor4f(1.0F, 1.0F, 1.0F, 1.0F);
-        this.mc.getTextureManager().bindTexture(new ResourceLocation("kineticraft:textures/gui/stoneKineticEnergyCubeGui.png"));
-        
-        int k = (this.width - this.xSize) / 2;
-        int l = (this.height - this.ySize) / 2;
-        this.drawTexturedModalRect(k, l, 0, 0, this.xSize, 6 * 18 + 17);
-        this.drawTexturedModalRect(k, l + 6 * 18 + 17, 0, 125, this.xSize, 96);
-    }
 }
